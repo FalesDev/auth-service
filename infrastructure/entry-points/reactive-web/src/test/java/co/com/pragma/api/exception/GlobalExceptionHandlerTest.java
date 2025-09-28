20a34e8f1b0b5987763e32db3127937dba2feeb3
FalesDev <fabriciorodriguezavalos043@gmail.com>
2025-09-28 17:00:26 -0500

    Tests faltantes en globalexceptionhandler

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/pragma/api/exception/GlobalExceptionHandlerTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/pragma/api/exception/GlobalExceptionHandlerTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
